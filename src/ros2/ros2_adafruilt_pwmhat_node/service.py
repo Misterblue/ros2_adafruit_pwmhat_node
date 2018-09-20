75f932a4c3685ba0fcf06fe39b1356074d3e3b2e
Robert Adams <misterblue@misterblue.com>
2018-09-20 13:35:19 -0700

    Rename package so it's all lowercase and uses underscores instead of hyphens.

--- src/ros2/ros2_adafruilt_pwmhat_node/service.py
+++ src/ros2/ros2_adafruilt_pwmhat_node/service.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from svc import BasicPWMChannels
+import BasicPWMChannels
 
 import rclpy
 from rclpy.node import Node
