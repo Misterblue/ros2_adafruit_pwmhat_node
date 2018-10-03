67d14600294d10c9963890687c15ae8a3e7c1515
Robert Adams <misterblue@misterblue.com>
2018-10-03 09:48:10 -0700

    The service took a request and processed it. <manical-mad-scientist-voice>IT's ALIVE!!</manical-mad-scientist-voice>

--- src/ros2/ros2_adafruit_pwmhat_node/ros2_adafruit_pwmhat_node/__init__.py
+++ src/ros2/ros2_adafruit_pwmhat_node/ros2_adafruit_pwmhat_node/__init__.py
@@ -14,29 +14,42 @@
 
 import sys
 
-import Adafruit_PCA9685
-from ros2_adafruit_pwmhat_msgs.srv import BasicPWMchannels
-
 import rclpy
 from rclpy.node import Node
+
+import Adafruit_PCA9685
+
+from ros2_adafruit_pwmhat_msgs.srv import BasicPWMchannels
 
 class ROS2_Adafruit_pwmhat_node(Node):
 
     def __init__(self):
         super().__init__('ros2_adafruit_pwmhat_node')
-        self.srv = self.create_service(BasicPWMchannels, 'basicpwmchannels', self.basicpwmchannels_callback)
+        self.srv = self.create_service(BasicPWMchannels, 'pwmhat/pulselength', self.basicpwmchannels_callback)
         self.pwm = Adafruit_PCA9685.PCA9685()
-        print('sys.path=')
-        for path in sys.path:
-            print(path)
+        # frequency can be from 40 to 1000
+        self.pwm_frequency = 60
+        self.pwm.set_pwm_freq(self.pwm_frequency)
 
     def basicpwmchannels_callback(self, request, response):
-        self.get_logger().info('Incoming request: chan=%d pulse_length=%d' % (request.chan, request.pulse_length))
-        self.pwm.set_pwm(request.chan, 0, request.pulse_length)
+        # self.get_logger().info(f"Incoming request: chan={request.chan} pulse_length={request.pulse_length}")
+        self.get_logger().info("Incoming request: chan=%s pulse_length=%s" % (request.chan, request.pulse_length) )
+
+        self.set_pwm_by_pulse_length(request.chan, request.pulse_length)
         
         response.error = 0
 
         return response
+
+    def set_pwm_by_pulse_length(self, pin, pulse_length):
+        # turn specified PWM channel on for the given pulse_length in microseconds
+        pulse_scaler = 1000000              # microseconds
+        pulse_scaler //= self.pwm_frequency # microseconds per cycle
+        pulse_scaler //= 4096               # microseconds per PWM count
+        conv_pulse_length = int((pulse_length * 1000) / pulse_scaler)
+        conv_pin = int(pin)
+        # pwm.set_pwm(channel, onTickTime, offTickTime) where on/off are within 0..4095
+        self.pwm.set_pwm(conv_pin, 0, conv_pulse_length)
 
 
 def main(args=None):
