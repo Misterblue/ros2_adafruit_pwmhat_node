8e96194513e1c8ab97f9071101629301209e7f46
Robert Adams <misterblue@misterblue.com>
2018-10-05 18:15:14 -0700

    Change node from a server to just a subscriber.  This fits more with ROS design. Remove srv definitions and add msg definitions. Code for topic subscription and message processing. Add parameterable pin definitions. Add namespace specification which can be modified on invocation command line.

--- src/ros2/ros2_adafruit_pwmhat_node/ros2_adafruit_pwmhat_node/ros2_adafruit_pwmhat_node.py
+++ src/ros2/ros2_adafruit_pwmhat_node/ros2_adafruit_pwmhat_node/ros2_adafruit_pwmhat_node.py
@@ -19,46 +19,127 @@ from rclpy.node import Node
 
 import Adafruit_PCA9685
 
-from ros2_adafruit_pwmhat_msgs.srv import PWMPulseLength
-from ros2_adafruit_pwmhat_msgs.srv import PWMAngle
+from ros2_adafruit_pwmhat_msgs.msg import PWMPinPulseLength, PWMPulseLength
+from ros2_adafruit_pwmhat_msgs.msg import PWMPinAngle, PWMAngle
 
 class ROS2_Adafruit_pwmhat_node(Node):
+    # Node for driving Adafruit PWM hat for the Raspberry Pi 3
+    # Subscribes to multiple topics to accept settings for named pins and for
+    #    setting values in both raw pulse length and angle.
 
     def __init__(self):
-        super().__init__('ros2_adafruit_pwmhat_node')
+        super().__init__('ros2_adafruit_pwmhat_node', namespace='pwmhatter')
+
         self.pwm = Adafruit_PCA9685.PCA9685()
         # frequency can be from 40 to 1000
-        self.pwm_frequency = 60
+        self.pwm_frequency = 100
         self.pwm.set_pwm_freq(self.pwm_frequency)
 
-        self.srv = self.create_service(PWMPulseLength, 'pwmhat/pulselength', self.pulselength_callback)
-        self.srv = self.create_service(PWMAngle, 'pwmhat/angle', self.angle_callback)
+        # subscriptions for different message types (named, pins, angle)
+        self.subs = []
+        self.subs.append(self.create_subscription(
+                    PWMPinAngle, 'pinAngle', self.pinAngle_callback))
+        self.subs.append(self.create_subscription(
+                    PWMAngle, 'angle', self.angle_callback))
+        self.subs.append(self.create_subscription(
+                    PWMPinPulseLength, 'pinPulseLength', self.pinPulseLength_callback))
+        self.subs.append(self.create_subscription(
+                    PWMPulseLength, 'pulseLength', self.pulseLength_callback))
 
-    def pulselength_callback(self, request, response):
-        self.get_logger().info("Incoming request: chan=%s pulse_length=%s" % (request.chan, request.pulse_length) )
+        # THE FOLLOWING DATA WILL COME FROM PARAMETER FILES WHEN THAT WORKS FOR ROS2 AND PYTHON
 
-        self.set_pwm_by_pulse_length(request.chan, request.pulse_length)
-        
-        response.error = 0
+        # parameters for each type of device that can be connected to a pin
+        self.device_types = {}
+        self.device_types['SG90'] = {
+            'name': 'SG90',
+            'range_degrees': [ -90, 90 ],
+            # documentation says 1..2ms but experimentation says the range is a little larger
+            'min_pulse_length': 0.6,    # pulse length for -90 degrees
+            'max_pulse_length': 2.4     # pulse length for +90 degrees
+            }
 
-        return response
+        # what's connected to each pin
+        self.pins = []
+        for ii in range(0,15):
+            self.pins.append( { 'pin': ii, 'device_type': 'SG90' } )
+        self.pins[8]['channel'] = 'tilt'
+        self.pins[9]['channel'] = 'pan'
 
-    def angle_callback(self, request, response):
-        # set PWM based on angle
-        self.get_logger().info("Incoming request: chan=%s angle=%s" % (request.chan, request.angle) )
+        # map channel names to pin numbers
+        self.channels = {}
+        for pinIndex in range(len(self.pins)):
+            aPin = self.pins[pinIndex]
+            if 'channel' in aPin:
+                self.channels[aPin['channel']] = pinIndex
 
-        if request.angle >= -90 and request.angle <= 90:
-            angle = request.angle + 90
-
-            low_bound = 1.0  # 0.6
-            high_bound = 2.0 # 2.4
-            calc_pulse_length = (((high_bound - low_bound) / 180) * angle) + low_bound
-            self.set_pwm_by_pulse_length(request.chan, calc_pulse_length)
-            response.error = 0
+    def angle_callback(self, request):
+        # set PWM based on channel name and angle
+        self.get_logger().debug("PWMHat: chan/angle: chan=%s angle=%s"
+                    % (request.chan, request.angle) )
+        if request.chan in self.channels:
+            pin = self.channels[request.chan]
+            pulse_length = convert_angle_to_pulse_length(pin, request.angleUnits, request.angle)
+            self.set_pwm_by_pulse_length(pin, pulse_length)
         else:
-            response.error = 1
+            self.get_logger().error("PWMHAt: chan/angle: channel does not exist: chan=%s, angle=%s"
+                    % (request.chan, request.angle) )
+        return
+        
+    def pinAngle_callback(self, request):
+        # set PWM based on pin and angle
+        self.get_logger().debug("PWMHat: pin/angle: pin=%s angle=%s"
+                    % (request.pin, request.angle) )
+        if pin >= 0 and pin < len(self.pins):
+            pulse_length = convert_angle_to_pulse_length(request.pin, request.angleUnits, request.angle)
+            self.set_pwm_by_pulse_length(request.pin, pulse_length)
+        else:
+            self.get_logger().error("PWMHAt: pin/angle: pin out of range: pin=%s, angle=%s"
+                    % (request.pin, request.angle) )
+        return
 
-        return response
+    def pulseLength_callback(self, request):
+        # set PWM based on channel and pulse length
+        self.get_logger().debug("PWMHat: chan/pulseLength: chan=%s pulse_length=%s"
+                    % (request.chan, request.pulse_length) )
+        if request.chan in self.channels:
+            pin = self.channels[request.chan]
+            self.set_pwm_by_pulse_length(pin, request.pulseLength)
+        else:
+            self.get_logger().error("PWMHAt: chan/pulseLength: channel does not exist: chan=%s, pulseLength=%s"
+                    % (request.chan, request.pulseLength) )
+        return
+
+    def pinPulseLength_callback(self, request):
+        # set PWM based on pin number and pulse length
+        self.get_logger().debug("PWMHAT: pin/pulseLength: pin=%s pulse_length=%s"
+                    % (request.pin, request.pulse_length) )
+        if pin >= 0 and pin < len(self.pins):
+            self.set_pwm_by_pulse_length(request.pin, request.pulseLength)
+        else:
+            self.get_logger().error("PWMHAt: pin/pulseLength: pin out of range: pin=%s, angle=%s"
+                    % (request.pin, request.angle) )
+        return
+
+    def convert_angle_to_pulse_length(self, pin, units, angle):
+        # given an angle and a pin description, return pulse length
+        calc_pulse_length = 1.0
+        conv_angle = angle
+        if units == PWMAngle.RADIANS:
+            conv_angle = angle * (180 / 3.14159265)
+
+        pinInfo = self.pins[pin]
+        deviceType = self.deviceTypes[pinInfo['device_type']]
+        if conv_angle >= deviceType.range[0] and conv_angle <= deviceType.range[1]:
+            # if angle is in range, adjust the range to be zero based
+            conv_angle -= deviceType.range[0]
+            calc_pulse_length = (deviceType['max_pulse_length'] - deviceType['min_pulse_length']) / 180
+            calc_pulse_length *= angle
+            calc_pulse_length += deviceType['min_pulse_length']
+        else:
+            self.get_logger().error("PWMHat: Pulse length calc: angle out of bounds: pin=%s, angle=%s"
+                    % (pin, angle) )
+
+        return calc_pulse_length
 
     def set_pwm_by_pulse_length(self, pin, pulse_length):
         # turn specified PWM channel on for the given pulse_length in microseconds
@@ -68,16 +149,16 @@ class ROS2_Adafruit_pwmhat_node(Node):
         conv_pulse_length = int((pulse_length * 1000) / pulse_scaler)
         conv_pin = int(pin)
         # pwm.set_pwm(channel, onTickTime, offTickTime) where on/off are within 0..4095
-        self.get_logger().info("set_pwm: pin=%s conv_pulse_length=%s" % (conv_pin, conv_pulse_length) )
+        self.get_logger().debug("set_pwm: pin=%s conv_pulse_length=%s" % (conv_pin, conv_pulse_length) )
         self.pwm.set_pwm(conv_pin, 0, conv_pulse_length)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    pwmService = ROS2_Adafruit_pwmhat_node()
+    pwmNode = ROS2_Adafruit_pwmhat_node()
 
-    rclpy.spin(pwmService)
+    rclpy.spin(pwmNode)
 
     rclpy.shutdown()
 
